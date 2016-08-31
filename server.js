13cf90f6974c41da44085e9841bde825ae0bcf34
Jorge <jcarapia83@Gmail.com>
2016-08-31 00:26:32 -0700

    video chat working locally

--- server.js
+++ server.js
@@ -1,14 +1,37 @@
-var express = require('express');
+var express = require('express.io');
 var app = express();
+app.http().io();
+var PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
 	res.render('index.ejs');
 });
 
-var PORT = process.env.PORT || 3000
+app.io.route('ready', function(req){
+	req.io.join(req.data.chat_room);
+	req.io.join(req.data.signal_room);
+	app.io.room(req.data).broadcast('announce', {
+		message: 'New client in the ' + req.data + 'room'
+	})
+})
 
-app.listen(PORT)
+app.io.route('send', function(req){
+	app.io.room(req.data.room).broadcast('message', {
+		message: req.data.message,
+		author: req.data.author
+	});
+})
+
+app.io.route('signal', function(req) {
+	//note the use of req here for broadcasting so only the sender doesn't receive their own messages
+	req.io.room(req.data.room).broadcast('signaling_message', {
+		type: req.data.type, 
+		message: req.data.message
+	});
+})
+
+app.listen(PORT);
 
 console.log('The magic happens on port ' + PORT);
